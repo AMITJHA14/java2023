b089ce0fc6480d2f9cdebe55e175cfe3cc0c31cc
AMITK <AMITK@MSI.mshome.net>
2023-07-22 21:54:21 +0530

    if else

---
+++ core/src/core/oddeven.java
@@ -0,0 +1,33 @@
+package core;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class oddeven{
+	
+	
+	
+	public static void main(String[] args) {
+	         
+	 
+	}
+	public static int oddEven(int a) {
+		if (a%2 !=1) {
+			System.out.println("even");
+			
+		}
+		else {
+			System.out.println("odd");
+		}
+		return a;
+		
+	}
+	}
+	
+	
+
