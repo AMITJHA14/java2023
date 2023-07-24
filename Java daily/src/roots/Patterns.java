20394a9bc3ea51c9ee2320f4e8714c01090504e8
AMITK <AMITK@MSI.mshome.net>
2023-07-24 22:00:42 +0530

    Star

--- Java daily/src/roots/Patterns.java
+++ Java daily/src/roots/Patterns.java
@@ -4,76 +4,104 @@ public class Patterns {
 
 	public static void main(String[] args) {
 
+	
+		System.out.println(
+				"____________________________________________________________________________________________________");
 		System.out.println("1)");
-		for (int i = 1; i <= 5; i++) {
-			for (int j = 1; j <= 5; j++) {
-				System.out.print("* ");
+		
+		
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
 			}
-
-			System.out.println("");
+		System.out.println("");
 		}
+		
+		
+
 		System.out.println(
 				"____________________________________________________________________________________________________");
 		System.out.println("2)");
 
-		for (int i = 1; i <= 5; i++) {
-			for (int j = 1; j <= 5; j++) {
-				System.out.print("*");
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
 			}
-			System.out.println("");
+		 System.out.println("");
 		}
-
+		
+		
 		System.out.println(
 				"____________________________________________________________________________________________________");
 		System.out.println("3)");
 
-		for (int i = 1; i <= 4; i++) {
-			for (int j = 1; j <= i; j++) {
-				System.out.print("* ");
+		
+		for(int i=1;i<=4;i++) {
+			for(int j=4;j>=i;j--) {
+				System.out.print("*");
+		
 			}
 			System.out.println("");
-		}
-
+	}
+		
+		
 		System.out.println(
 				"____________________________________________________________________________________________________");
 		System.out.println("4)");
 
-		for (int i = 1; i <= 4; i++) {
-			for (int j = 1; j <= i; j++) {
-				System.out.print("*");
+		for(int i=1;i<=4;i++) {
+			for(int j=4;j>=i;j--) {
+				System.out.print("* ");
+		
 			}
-			System.out.println("");
+		System.out.println(" ");
+		
 		}
-
+		
+	
 		System.out.println(
 				"____________________________________________________________________________________________________");
 		System.out.println("5)");
-
-		for (int i = 1; i <= 4; i++) {
-			for (int j = 4; j >= i; j--) {
+	
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=i;j++) {
 				System.out.print("*");
 			}
-
-			System.out.println("");
+		System.out.println("");
 		}
+		
+		
+		for(int a=1;a<=4;a++) {
+			for(int k=3;k>=a;k--) {
+				System.out.print("*");
+			}
+		System.out.println("");
+		}
+		
 
 		System.out.println(
 				"____________________________________________________________________________________________________");
 		System.out.println("6)");
 	
-	
-	for(int i=1;i<=4;i++) {
-		for(int j=4;j>=i;j--) {
-			System.out.print("* ");
-		}
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
 		System.out.println("");
+		}
+		for(int i=1;i<=4;i++) {
+			for(int k=3;k>=i;k--) {
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+	
+
+	
+	}
 	}
 	
 	
 	
-	
-	}
-	
-	
-	}
+
 
